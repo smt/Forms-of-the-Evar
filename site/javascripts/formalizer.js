9bf985fc9a12024414b25503dc954f39781cfdf0
Vick Aita <vickaita@gmail.com>
2010-07-18 22:48:28 -0400

    more work on everything

--- site/javascripts/formalizer.js
+++ site/javascripts/formalizer.js
@@ -1,14 +1,18 @@
 var Formalizer = Formalizer || {};
+Formalizer.UI_BASE_SPEED = 50;
 
 Formalizer.initToggleMetaSections = function() {
         var hiddenClass = "ui-meta-hidden";
-        $('.meta a.toggle').live(function() {
-                var $parent = $(this).closest(".meta");
-                var $sections = $(".section", $parent);
+        $('.meta a.toggle').live('click', function() {
+                var $link = $(this);
+                var $parent = $link.closest(".meta");
+                var $s = $(".section", $parent);
                 if ( $parent.hasClass(hiddenClass) ) {
-                        $sections.slideUp();
+                        $s.slideDown(Formalizer.UI_BASE_SPEED * 5);
+                        $link.text("Close");
                 } else {
-                        $sections.slideDown();
+                        $s.slideUp(Formalizer.UI_BASE_SPEED * 5);
+                        $link.text("Open");
                 }
                 $parent.toggleClass(hiddenClass);
                 return false;
@@ -16,7 +20,21 @@ Formalizer.initToggleMetaSections = function() {
 };
 
 Formalizer.initAddField = function() {
+        var $form = $('#form-wrapper form');
+        var $template = $('#field-template');
+        $('.add-field').live('click', function() {
+                var data = { inputID: "first-name", labelText: "First Name" };
+                $template.render(data).appendTo($form);
+                return false;
+        });
 };
 
 Formalizer.initRemoveField = function() {
+        return false;
 };
+
+$(document).ready(function() {
+        Formalizer.initToggleMetaSections();
+        Formalizer.initAddField();
+        Formalizer.initRemoveField();
+});
